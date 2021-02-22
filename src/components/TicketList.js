fd98929eaef17873f2cf2bb4e2008535ec135d82
Jerrod Styrk <jstyrk@citadel.edu>
2021-02-22 14:59:34 -0800

    add firebase functionality across our delete, edit, add functions
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,13 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ticket from "./Ticket";
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function TicketList(props){
-  return (
-    <React.Fragment>
-      <hr/>
-      {Object.values(props.ticketList).map((ticket) => {
-        return <Ticket
+  useFirestoreConnect([
+    { collection: 'tickets' }
+  ]);
+
+  const tickets = useSelector(state => state.firestore.ordered.tickets);
+  
+  if (isLoaded(tickets)) {
+
+      return (
+        <React.Fragment>
+        <hr/>
+        {tickets.map((ticket) => {
+          return <Ticket
           whenTicketClicked = { props.onTicketSelection }
           names={ticket.names}
           location={ticket.location}
@@ -15,13 +25,19 @@ function TicketList(props){
           formattedWaitTime={ticket.formattedWaitTime}
           id={ticket.id}
           key={ticket.id}/>
-      })}
-    </React.Fragment>
-  );
+        })}
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.object,
   onTicketSelection: PropTypes.func
 };
 
